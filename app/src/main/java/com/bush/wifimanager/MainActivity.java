6741dea03ff98a137bfebade5af69b1e2df0b833
bush261800 <Sha@12345678>
2020-05-06 18:55:12 +0530

    Added Null check for WifiManager Service

--- app/src/main/java/com/bush/wifimanager/MainActivity.java
+++ app/src/main/java/com/bush/wifimanager/MainActivity.java
@@ -5,40 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_wifi_on,btn_wifi_off;
+    private static final String TAG = "MainActivity";
+    Button btnWifiOn, btnwifioff;
     WifiManager wifiManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_wifi_on=(Button) findViewById(R.id.btnTurnOn);
-        btn_wifi_off=(Button) findViewById(R.id.btnTurnOff);
+        btnWifiOn = findViewById(R.id.btnTurnOn);
+        btnwifioff = findViewById(R.id.btnTurnOff);
 
 
-        btn_wifi_on.setOnClickListener(new View.OnClickListener() {
+        btnWifiOn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-                wifiManager.setWifiEnabled(true);
+                if (wifiManager != null) {
+                    wifiManager.setWifiEnabled(true);
+                    Log.d(TAG, "onClick: Wifi enabled");
+                }
                 Toast.makeText(MainActivity.this, "Wifi Turn On", Toast.LENGTH_SHORT).show();
 
             }
         });
 
-        btn_wifi_off.setOnClickListener(new View.OnClickListener() {
+        btnwifioff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-                wifiManager.setWifiEnabled(false);
+                if (wifiManager != null) {
+                    wifiManager.setWifiEnabled(false);
+                    Log.d(TAG, "onClick: Wifi disabled");
+                }
                 Toast.makeText(MainActivity.this, "Wifi Turn OFF", Toast.LENGTH_SHORT).show();
 
 
